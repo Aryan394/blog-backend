52f1441e97bb9d19edc62f99a84bd2ae2b88d964
Aryan Singh <80955630+Aryan394@users.noreply.github.com>
2023-07-24 12:33:31 +0530

    fixed CORS error

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ const uploadMiddleware = multer({ dest: "uploads/" });
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000/" })); //if we are using credentials, we need to specify some information in cors
+app.use(cors({
+  'allowedHeaders': ['Content-Type'],
+  'origin': '*',
+  'preflightContinue': true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
